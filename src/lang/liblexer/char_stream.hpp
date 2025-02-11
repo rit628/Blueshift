b79d56074e157ff3c8b1590f1cb753e1c6eb0f9d
Tomas Rivera <tr4462603@gmail.com>
2025-02-10 19:44:13 -0500

    liblexer: Update peek and match functions to support characters in addition to regular expressions

--- src/lang/liblexer/char_stream.hpp
+++ src/lang/liblexer/char_stream.hpp
@@ -1,12 +1,16 @@
 #pragma once
 #include "token.hpp"
+#include <concepts>
 #include <cstddef>
-#include <functional>
-#include <initializer_list>
 #include <sstream>
 #include <boost/regex.hpp>
 
 namespace BlsLang {
+
+    template<typename T>
+    concept CharacterPattern = std::same_as<T, char>
+                            || std::same_as<T, boost::regex>;
+
     class CharStream {
         public:
             CharStream() {}
@@ -14,9 +18,11 @@ namespace BlsLang {
 
             void loadSource(const std::string& input);
             // Looks ahead in stream (without consuming characters) for characters matching given patterns
-            bool peek(std::initializer_list<std::reference_wrapper<const boost::regex>> patterns);
+            template<CharacterPattern... Args>
+            bool peek(Args... patterns);
             // Looks ahead in stream for characters matching given patterns and consumes matching characters
-            bool match(std::initializer_list<std::reference_wrapper<const boost::regex>> patterns);
+            template<CharacterPattern... Args>
+            bool match(Args... patterns);
             void resetToken();
             // Creates a token with the currently consumed characters
             Token emit(Token::Type type);
@@ -25,8 +31,54 @@ namespace BlsLang {
             size_t getLine() const { return line; }
             size_t getColumn() const { return col; }
         private:
+            template<CharacterPattern T>
+            bool peekPattern(T pattern);
+
             std::istringstream ss;
             std::string currToken = "";
             size_t line = 1, col = 1, tokenLine = 1, tokenCol = 1;
     };
+
+    template<CharacterPattern... Args>
+    inline bool CharStream::peek(Args... patterns) {
+        size_t initIdx = ss.tellg();
+        auto result = (peekPattern(std::forward<Args>(patterns)) && ...);
+        ss.seekg(initIdx); // return to start
+        return result;
+    }
+
+    template<CharacterPattern... Args>
+    inline bool CharStream::match(Args... patterns) {
+        if (!peek(patterns...)) return false;
+        size_t numPatterns = sizeof...(patterns);
+        char currChar;
+        for (size_t i = 0; i < numPatterns; i++) {
+            currChar = ss.get();
+            col++;
+            currToken += currChar;
+            if (currChar == '\n') {
+                line++;
+                col = 1;
+            }
+        }
+        return true;
+    }
+
+    template<CharacterPattern T>
+    inline bool CharStream::peekPattern(T pattern) {
+        if constexpr (std::is_same<T, char>()) {
+            if (ss.eof() || ss.peek() != pattern) {
+                return false;
+            }
+        }
+        else {
+            std::string currChar; 
+            currChar = ss.peek();
+            if (ss.eof() || !boost::regex_match(currChar, pattern)) {
+                return false;
+            }
+        }
+        ss.seekg(1, ss.cur); // move forward in stream
+        return true;
+    }
 }
--- src/lang/liblexer/include/token_definitions.hpp
+++ src/lang/liblexer/include/token_definitions.hpp
@@ -3,37 +3,37 @@
 
 namespace BlsLang {
 
-    const boost::regex IDENTIFIER_START             (R"([A-Za-z])");
-    const boost::regex IDENTIFIER_END               (R"([A-Za-z0-9_])");
+    const static boost::regex IDENTIFIER_START                      (R"([A-Za-z])");
+    const static boost::regex IDENTIFIER_END                        (R"([A-Za-z0-9_])");
 
-    const boost::regex NUMERIC_DIGIT                (R"([0-9])");
-    const boost::regex ZERO                         (R"(0)");
-    const boost::regex OCTAL_DIGIT                  (R"([0-7])");
-    const boost::regex HEX_START                    (R"([xX])");
-    const boost::regex HEX_DIGIT                    (R"([0-9a-fA-F])");
-    const boost::regex BINARY_START                 (R"([bB])");
-    const boost::regex BINARY_DIGIT                 (R"([0-1])");
-    const boost::regex NEGATIVE_SIGN                (R"(-)");
-    const boost::regex DECIMAL_POINT                (R"(\.)");
+    const static char ZERO                                          ('0');
+    const static char NEGATIVE_SIGN                                 ('-');
+    const static char DECIMAL_POINT                                 ('.');
+    const static boost::regex NUMERIC_DIGIT                         (R"([0-9])");
+    const static boost::regex OCTAL_DIGIT                           (R"([0-7])");
+    const static boost::regex HEX_START                             (R"([xX])");
+    const static boost::regex HEX_DIGIT                             (R"([0-9a-fA-F])");
+    const static boost::regex BINARY_START                          (R"([bB])");
+    const static boost::regex BINARY_DIGIT                          (R"([0-1])");
 
-    const boost::regex ESCAPE_SLASH                 (R"(\\)");
-    const boost::regex ESCAPE_CHARS                 (R"([bnrt'\"\\])");
+    const static char ESCAPE_SLASH                                  ('\\');
+    const static boost::regex ESCAPE_CHARS                          (R"([bnrt'\"\\])");
 
-    const boost::regex STRING_QUOTE                 (R"(\")");
-    const boost::regex STRING_LITERALS              (R"([^\"\n\r\\])"); // or escapes
+    const static char STRING_QUOTE                                  ('"');
+    const static boost::regex STRING_LITERALS                       (R"([^\"\n\r\\])"); // or escapes
 
-    const boost::regex COMMENT_SLASH                ("/");
-    const boost::regex COMMENT_STAR                 (R"(\*)");
-    const boost::regex COMMENT_CONTENTS_SINGLELINE  (R"([^\n])");
-    const boost::regex COMMENT_CONTENTS_MULTILINE   (R"(.)");
+    const static char COMMENT_SLASH                                 ('/');
+    const static char COMMENT_STAR                                  ('*');
+    const static boost::regex COMMENT_CONTENTS_SINGLELINE           (R"([^\n])");
+    const static boost::regex COMMENT_CONTENTS_MULTILINE            (R"(.)");
 
-    const boost::regex OPERATOR_EQUALS_PREFIX       (R"([<>!=+\-*/^%])");
-    const boost::regex OPERATOR_EQUALS              (R"(\=)");
-    const boost::regex OPERATOR_PLUS                (R"(\+)");
-    const boost::regex OPERATOR_MINUS               (R"(\-)");
-    const boost::regex OPERATOR_AND                 (R"(\&)");
-    const boost::regex OPERATOR_OR                  (R"(\|)");
-    const boost::regex OPERATOR_GENERIC             (R"(.)");
+    const static char OPERATOR_EQUALS                               ('=');
+    const static char OPERATOR_PLUS                                 ('+');
+    const static char OPERATOR_MINUS                                ('-');
+    const static char OPERATOR_AND                                  ('&');
+    const static char OPERATOR_OR                                   ('|');
+    const static boost::regex OPERATOR_EQUALS_PREFIX                (R"([<>!=+\-*/^%])");
+    const static boost::regex OPERATOR_GENERIC                      (R"(.)");
 
-    const boost::regex WHITESPACE                   ("[ \u000B\u0008\\n\\r\\f\\t]"); // not a raw string to include unicode characters
+    const static boost::regex WHITESPACE                            ("[ \u000B\u0008\\n\\r\\f\\t]"); // not a raw string to include unicode characters
 }
