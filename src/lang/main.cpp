3405d2afae37415a216d0c201b2f4b0004f2773e
Tomas Rivera <tr4462603@gmail.com>
2025-03-06 10:58:44 -0500

    libanalyzer: Add initial type checking for return statements

--- src/lang/main.cpp
+++ src/lang/main.cpp
@@ -1,25 +1,15 @@
+#include "bls_types.hpp"
+#include "libanalyzer/analyzer.hpp"
 #include "libinterpreter/interpreter.hpp"
 #include "liblexer/lexer.hpp"
 #include "libparser/parser.hpp"
 #include <fstream>
 #include <iostream>
 #include <sstream>
-#include <string>
 
 int main() {
     // sample engine
     BlsLang::Lexer lexer;
-    std::string test = \
-    R"(a bunch of text on multiple
-    lines with some // comments
-    that are //singleline and
-    multiline as follows /* some multiline
-    comment that finishes just
-    about here */ now after that lest test some
-    strings like this one "some text \n \t " now
-    time for some numbers 1 12 -12 decimals 
-    too 1.2 .1 -1.2 -.3 -0.5 hooray now time for 
-    the ops <= >= != == += -= *= /= ^= %= ++ -- && || . () $ yipee!)";
     std::ifstream file;
     file.open("test/lang/samples/simple.blu");
     std::stringstream ss;
@@ -49,5 +39,6 @@ int main() {
             }
         }
     }
+
     return 0;
 }
