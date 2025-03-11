21933f8ad8bc9b19659ec894fe2cd3a9b5a56c06
Tomas Rivera <tr4462603@gmail.com>
2025-03-10 23:25:12 -0400

    tester: Fix testerMain

--- src/client/testerMain.cpp
+++ src/client/testerMain.cpp
@@ -1,21 +1,17 @@
 #include <fstream> 
+#include <iostream>
 
 int main(int argc, char** argv){
-    if(argc < 2){
+    if(argc < 3){
+        std::cerr << "invalid number of arguments provided" << std::endl;
         return 1; 
     }
-
-    
     
     std::ofstream ostream; 
-    ostream.open("./samples/client/file1.txt"); 
-    for(int i = 0; i < argc; i++){
-        ostream << argv[i+1]; 
-        /*
-        if(i != argc-1){
-            ostream << " "; 
-        }
-        */ 
+    ostream.open("./samples/client/" + std::string(argv[1])); 
+    ostream << argv[2];
+    for(int i = 2; i < argc; i++){
+        ostream << " " << argv[i+1];
     }
     ostream.close(); 
     
