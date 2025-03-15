3d9d83b4d736914221acf1c310ed0eecb9ad29b9
rit628 <tr4462603@gmail.com>
2025-03-15 19:23:07 -0400

    util: Add command line args to bsmp

--- src/util/bsm_printer/main.cpp
+++ src/util/bsm_printer/main.cpp
@@ -1,8 +1,11 @@
 #include "bytecode_printer.hpp"
 
-int main() {
-
-    BytecodePrinter bsmReader;
-    bsmReader.loadBytecode("./samples/bsm/test.bsm");
-    bsmReader.dispatch();
+int main(int argc, char** argv) {
+    if(argc < 2){
+        std::cerr << "invalid number of arguments provided" << std::endl;
+        return 1; 
+    }
+    BytecodePrinter bsmp;
+    bsmp.loadBytecode("./samples/bsm/" + std::string(argv[1]));
+    bsmp.dispatch();
 }
