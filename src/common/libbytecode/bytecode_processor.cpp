68d33e2e97fdeb5e0287f9b0bd7be686e25cd9c1
rit628 <tr4462603@gmail.com>
2025-04-16 14:30:08 -0400

    libbytecode: Add support for out of order execution

--- src/common/libbytecode/bytecode_processor.cpp
+++ src/common/libbytecode/bytecode_processor.cpp
@@ -15,14 +15,17 @@ void BytecodeProcessor::dispatch() {
     char buf[sizeof(uint16_t) + 1];
     while (bytecode.get(buf[0])) {
         OPCODE code = static_cast<OPCODE>(buf[0]);
+        uint16_t currAddress;
         switch (code) {
             #define OPCODE_BEGIN(code) \
             case OPCODE::code: {
             #define ARGUMENT(arg, type) \
                 type arg; \
                 bytecode.read(buf, sizeof(type)); \
-                std::memcpy(&arg, buf, sizeof(type));
+                std::memcpy(&arg, buf, sizeof(type)); \
+                instruction += sizeof(type);
             #define OPCODE_END(code, args...) \
+                currAddress = ++instruction; \
                 code(args); \
             break; \
             } 
@@ -31,8 +34,12 @@ void BytecodeProcessor::dispatch() {
             #undef ARGUMENT
             #undef OPCODE_END
             default:
+                currAddress = ++instruction;
                 std::cerr << "INVALID OPCODE" << std::endl;
             break;
+        }
+        if (currAddress != instruction) { // instruction ptr modified by opcode (JMP, CALL, etc.)
+            bytecode.seekg(instruction);
         }
     }
 }
