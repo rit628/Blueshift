7d4ac66124fcd1a3b1b3b841f0f9fbfc9dad6f05
rit628 <tr4462603@gmail.com>
2025-04-17 11:50:37 -0400

    util: Add literal pool pretty printing

--- src/util/bsmp/bytecode_printer.cpp
+++ src/util/bsmp/bytecode_printer.cpp
@@ -23,6 +23,92 @@ void tag_invoke(const value_from_tag&, value& jv, std::shared_ptr<HeapDescriptor
     }
 }
 
+static void pretty_print( std::ostream& os, value const& jv, bool init = false)
+{
+    switch(jv.kind())
+    {
+    case kind::object:
+    {
+        os << "{";
+        auto const& obj = jv.get_object();
+        if(! obj.empty())
+        {
+            auto it = obj.begin();
+            for(;;)
+            {
+                os << serialize(it->key()) << " : ";
+                pretty_print(os, it->value());
+                if(++it == obj.end())
+                    break;
+                os << ", ";
+            }
+        }
+        os << "}";
+        break;
+    }
+
+    case kind::array:
+    {
+        os << "[";
+        if (init) {
+            os << "\n";
+        }
+        auto const& arr = jv.get_array();
+        if(! arr.empty())
+        {
+            auto it = arr.begin();
+            for(;;)
+            {
+                pretty_print( os, *it);
+                if(++it == arr.end())
+                    break;
+                if (init) {
+                    os << ",\n";                    
+                }
+                else {
+                    os << ", ";
+                }
+            }
+        }
+        if (init) {
+            os << "\n";
+        }
+        os << "]";
+        break;
+    }
+
+    case kind::string:
+    {
+        os << serialize(jv.get_string());
+        break;
+    }
+
+    case kind::uint64:
+    case kind::int64:
+        os << jv;
+        break;
+    case kind::double_:
+    {
+        double temp = value_to<double>(jv);
+        os << temp;
+        break;
+    }
+    case kind::bool_:
+        if(jv.get_bool())
+            os << "true";
+        else
+            os << "false";
+        break;
+
+    case kind::null:
+        os << "null";
+        break;
+    }
+
+    if(init)
+        os << "\n";
+}
+
 static std::ostream& operator<<(std::ostream& os, uint8_t& num) {
     os << static_cast<uint16_t>(num);
     return os;
@@ -38,7 +124,7 @@ void BytecodePrinter::printHeader() {
 
 void BytecodePrinter::printLiteralPool() {
     auto json = value_from(literalPool);
-    *outputStream << json << std::endl;
+    pretty_print(*outputStream, json, true);
     *outputStream << "BYTECODE_BEGIN" << std::endl;
 }
 
