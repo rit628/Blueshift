95160a9dfe8cc290df01675766033f7656405118
Rohan Rao <rohanrao.mail@gmail.com>
2025-06-14 10:53:27 -0400

    Symgraph + Divider: Updated Divider with the Code Division Algorithm

--- src/lang/libdivider/divider.cpp
+++ src/lang/libdivider/divider.cpp
@@ -1,11 +1,209 @@
 #include "divider.hpp"
 #include "ast.hpp"
+#include "symbolicator.hpp"
+#include <algorithm>
+#include <memory>
+#include <sys/stat.h>
 #include <variant>
 
 using namespace BlsLang; 
 
 BlsObject Divider::visit(AstNode::Source &ast){
+    // Open the blockStacks for the setup functions
+    for(auto& pair : this->DivMeta.ctlMetaData){
+        auto& ctlData = pair.second; 
+        auto srcCtl = std::make_unique<AstNode::Source>(); 
+        ControllerSource ControllerCarrier; 
+        ControllerCarrier.ctlSource = std::move(srcCtl); 
+        this->ctlSourceMap[pair.first] = std::move(ControllerCarrier); 
+    }
+ 
+    ast.getSetup()->accept(*this); 
+
+  
+    for(auto& OblockPtr : ast.getOblocks()){
+        this->inOblock = true; 
+        OblockPtr->accept(*this); 
+        this->inOblock = false; 
+    }
+  
+
+    // Populate the source of each object
+    for(auto& pair : this->DivMeta.ctlMetaData){
+        auto& ctlData = pair.second; 
+        auto& srcObj = this->ctlSourceMap[ctlData.ctlName];
+        auto& srcPtr = srcObj.ctlSource; 
+        srcPtr->getOblocks() = std::move(srcObj.oblock_list); 
+        
+        auto srcCtl = std::make_unique<AstNode::Source>(); 
+        srcCtl->getSetup()->getStatements() = std::move(srcObj.setup_statements); 
+    }
 
     return std::monostate(); 
-
 }
+
+BlsObject Divider::visit(AstNode::Setup &ast){
+
+    this->inSetup = true; 
+
+    for(auto& stmt : ast.getStatements()){
+        stmt->accept(*this); 
+    }
+
+    this->inSetup = false; 
+
+    return std::monostate(); 
+}
+
+BlsObject Divider::visit(AstNode::Function::Oblock &ast){
+    auto& ctlNames = this->DivMeta.OblockControllerSplit[ast.getName()]; 
+    OblockID ogOblockName = ast.getName(); 
+    
+  
+    for(auto& ctl: ctlNames){
+        OblockCopyInfo oci;
+        oci.oblockPtr = std::make_unique<AstNode::Function::Oblock>(); 
+        // Naming Schema used for now
+        OblockID oblockName = ogOblockName + "_" + ctl; 
+        oci.oblockPtr->getName() = oblockName; 
+        oci.blockStack.push({}); 
+
+        /* 
+            Fill out Oblock header data
+        */ 
+
+        DerivedOblock derOblock = this->DivMeta.ctlMetaData[ctl].oblockData[ast.getName()];
+        std::vector<std::string> newParameters; 
+        std::vector<std::unique_ptr<AstNode::Specifier::Type>> newParameterTypes; 
+
+        for(auto& dev : ast.getParameters()){
+            if(derOblock.devices.contains(dev)){    
+                newParameters.push_back(dev); 
+            } 
+        }
+        oci.oblockPtr->getParameters() = newParameters; 
+        this->oblockCopyMap.emplace(oblockName, std::move(oci)); 
+    }
+
+
+    for(auto& stmt : ast.getStatements() ){
+        stmt->accept(*this); 
+    }
+
+    for(auto& ctl : ctlNames){
+
+        OblockID oblockName = ogOblockName + "_" + ctl; 
+        auto& oPtr = this->oblockCopyMap[oblockName].oblockPtr; 
+        // Copy the statements for the divided oblock: 
+
+        auto& statementStack = this->oblockCopyMap[oblockName].blockStack; 
+        if(statementStack.size() == 1){
+            oPtr->getStatements() = std::move(statementStack.top()); 
+            statementStack.pop(); 
+        }
+        else{
+            std::cout<<"Divider Error: Statement stack at size: "<<statementStack.size()<<" when copied into Oblock!"<<std::endl; 
+        }
+        
+        this->ctlSourceMap.at(ctl).oblock_list.push_back(std::move(oPtr));
+        
+    }
+
+
+    return std::monostate(); 
+}
+
+
+
+
+
+BlsObject Divider::visit(AstNode::Statement::If &ast){
+    auto ctlSet = ast.getControllerSplit(); 
+
+    for(auto ctl : ctlSet){
+        auto oblockName = this->currOblock + "_" + ctl; 
+        this->oblockCopyMap[oblockName].blockStack.push({}); 
+    }
+
+    for(auto& stmt : ast.getBlock()){
+        stmt->accept(*this); 
+    }
+
+    for(auto& ctl : ctlSet){
+        // Pop and add the present controller split statement
+        auto oblockName = this->currOblock + "_" + ctl; 
+        auto& derOblockMap = this->oblockCopyMap[oblockName].blockStack; 
+        auto newStack = std::move(derOblockMap.top()); 
+        derOblockMap.pop(); 
+        
+        // make a new statement object
+        auto derivedIf = std::make_unique<AstNode::Statement::If>(); 
+        *derivedIf->getCondition() = *ast.getCondition();  
+        derivedIf->getBlock() = std::move(newStack); 
+    }
+
+
+   // Do the same for if else and else
+
+    return std::monostate(); 
+}
+
+BlsObject Divider::visit(AstNode::Statement::Declaration &ast){
+    auto ctls = ast.getControllerSplit();
+    
+    if(this->inSetup){
+        // Add the statement to the declaration
+
+        return std::monostate(); 
+    }
+
+    for(auto& ctlName : ctls){
+        auto OblockName = this->currOblock + "_" + ctlName;
+        auto splitDecl = std::make_unique<AstNode::Statement::Declaration>();
+
+        //*splitDecl = *ast; 
+        splitDecl->getName() = ast.getName(); 
+
+        auto& typeObj = ast.getType(); 
+        splitDecl->getType()->getName() = typeObj->getName(); 
+
+        
+        if(ast.getValue().has_value()){
+            *splitDecl->getValue()->get() = *ast.getValue()->get();
+        }
+        this->oblockCopyMap[OblockName].blockStack.top().push_back(std::move(splitDecl));
+    }
+
+    return std::monostate(); 
+}
+
+
+
+BlsObject Divider::visit(AstNode::Statement::Expression &ast){
+    if(this->inSetup){
+         // Add the statement to the declaration
+
+
+     
+        return std::monostate(); 
+    }
+
+
+    auto ctlList = ast.getControllerSplit(); 
+    for(auto& ctlName : ctlList){
+        auto OblockName = this->currOblock + "_" + ctlName; 
+        auto newExpr = std::make_unique<AstNode::Statement::Expression>(); 
+        *newExpr->getExpression() = *ast.getExpression(); 
+
+        this->oblockCopyMap[OblockName].blockStack.top().push_back(std::move(newExpr)); 
+    }
+    return std::monostate(); 
+}
+
+
+
+
+
+ 
+
+
--- src/lang/libdivider/divider.hpp
+++ src/lang/libdivider/divider.hpp
@@ -2,29 +2,108 @@
 #include "ast.hpp"
 #include "include/Common.hpp"
 #include "../libsymgraph/symgraph.hpp"
+#include <memory>
+#include <type_traits>
 #include <unordered_map>
+#include <unordered_set>
+
+
 
 
 
 namespace BlsLang{
+
+    // Derived Oblock Metadata for performing the split
+    struct DerivedOblock{
+        std::unordered_set<DeviceID> devices; 
+        OblockID oblockName; 
+        std::unordered_set<ControllerID> dividedCtls; 
+    }; 
+
+    struct ControllerMetadata{
+        // Oblock List
+        ControllerID ctlName; 
+        std::unordered_map<OblockID, DerivedOblock> oblockData; 
+        std::vector<DeviceID> devices;  
+    }; 
+
+    // Data from dep graph needed to function (in addition to the annotation)
+    struct DividerMetadata{
+        std::unordered_map<ControllerID, ControllerMetadata> ctlMetaData; 
+        std::unordered_map<OblockID ,std::unordered_set<ControllerID>> OblockControllerSplit; 
+    }; 
+
+
+    // Controller Source 
+    struct ControllerSource{
+        std::unique_ptr<AstNode::Source> ctlSource; 
+        std::vector<std::unique_ptr<AstNode::Statement>> setup_statements; 
+        std::vector<std::unique_ptr<AstNode::Function>> oblock_list;  
+    }; 
+
+
+    struct OblockCopyInfo{
+        std::stack<std::vector<std::unique_ptr<AstNode::Statement>>> blockStack; 
+        std::unordered_map<SymbolID,  bool> symbolDeclMap; 
+        std::unique_ptr<AstNode::Function::Oblock> oblockPtr;
+
+        OblockCopyInfo() = default; 
+
+        OblockCopyInfo(const OblockCopyInfo&) = delete;
+        OblockCopyInfo& operator=(const OblockCopyInfo&) = delete;
+
+        OblockCopyInfo(OblockCopyInfo&&) = default;
+        OblockCopyInfo& operator=(OblockCopyInfo&&) = default;
+
+    }; 
+
+
     class Divider : public Printer{
 
     private: 
-        std::unordered_map<ControllerID, AstNode::Source> CodeMap;  
-        std::unordered_set<ControllerID> AstNode; 
+        // What is to be returned by the divider
+        std::unordered_map<ControllerID, ControllerSource> ctlSourceMap;  
 
+        /*
+        * Divider Metadata (Produced by Symbolic Dependency Graph)
+        */
+
+        DividerMetadata DivMeta; 
+
+        /*
+
+        */
+        std::unordered_map<OblockID, OblockCopyInfo> oblockCopyMap; 
+        bool inOblock = true; 
+        OblockID currOblock; 
+        bool inSetup = false; 
+        // Used when visiting an expression node (with no access to splits)
+        OblockID subOblockName; 
+        
+        
     public: 
-        Divider():Printer(std::cout) {}
+        Divider():Printer(std::cout) {
+
+        }
 
         BlsObject visit(AstNode::Source& ast) override; 
+        BlsObject visit(AstNode::Setup &ast) override; 
         BlsObject visit(AstNode::Function::Oblock& ast) override; 
         BlsObject visit(AstNode::Statement::Declaration& ast) override;
         BlsObject visit(AstNode::Statement::Expression& ast) override; 
         BlsObject visit(AstNode::Statement::If &ast) override; 
+        BlsObject visit(AstNode::Statement::For &ast) override; 
+        BlsObject visit(AstNode::Statement::While &ast) override; 
+        BlsObject visit(AstNode::Expression::Binary &ast) override;
+        BlsObject visit(AstNode::Expression::Function &ast) override; 
         
-        BlsObject visit(AstNode::Expression::Access& ast) override; 
-        BlsObject visit(AstNode::Expression::Binary &ast) override; 
 
+        // Final Product
+        std::unordered_map<ControllerID, ControllerSource>& getControllerSplit(){
+            return this->ctlSourceMap; 
+        } 
+    
+    
 
 
     }; 
