c63f66a503c511384ae9e4580b5577af59cef2c0
Rohan Rao <rohanrao.mail@gmail.com>
2025-07-04 16:59:07 -0400

    Client: Added Client Gateway for Decentralization

--- src/client/libClientGateway/ClientGateway.hpp
+++ src/client/libClientGateway/ClientGateway.hpp
@@ -1,150 +1,79 @@
 #include <iostream>
 #include <memory>
 #include <unordered_map>
-#include <unordered_set>
+#include <vector>
 #include "include/Common.hpp"
-#include "libScheduler/scheduler.hpp"
+#include "libCE/Client.hpp"
+#include "libScheduler/Scheduler.hpp"
 #include "libTSQ/TSQ.hpp"
 #include "libProcessing/Processing.hpp"
-#include "libScheduler/Scheduler.hpp"
-#include "libnetwork/Connection.hpp"
 #include "libnetwork/Protocol.hpp"
+#include "../lang/libvirtual_machine/virtual_machine.hpp"
+#include "libbytecode/bytecode_processor.hpp"
+#include "libtype/bls_types.hpp"
+#include "libTSM/TSM.hpp"
 
-
-
-class ClientEU; 
-
-
-class ClientEUCache{
-    private: 
-        // Member Variables
-        TriggerManager triggerMan; 
-        std::unordered_map<DeviceID, DeviceBox> deviceInfo;
-        OblockID oblockName; 
-
-    public: 
-        ClientEUCache(OBlockDesc &odesc)
-        : triggerMan(odesc)
-        {
-            this->oblockName = odesc.name; 
-
-            for(DeviceDescriptor& desc : odesc.binded_devices){
-                DeviceID devID = desc.device_name; 
-                auto& devBox = this->deviceInfo[devID]; 
-                devBox.deviceName = desc.device_name;
-                devBox.devDropRead = desc.dropRead; 
-                devBox.stateQueues = std::make_shared<TSQ<DynamicMasterMessage>>(); 
-            }; 
-        }
-
-        // EMS state message
-        EMStateMessage grabStates(int priority)
-        {
-            EMStateMessage ems; 
-            std::vector<DynamicMasterMessage> dmmList; 
-
-            for(auto& pair : this->deviceInfo){
-                auto& devBox = pair.second; 
-                if(!devBox.stateQueues->isEmpty()){
-                    dmmList.push_back(devBox.stateQueues->read());
-                }
-                else{
-                    dmmList.push_back(devBox.lastMessage.get());
-                }
-            }
-
-            ems.dmm_list = dmmList; 
-            ems.oblockName = this->oblockName; 
-            ems.priority = priority; 
-            ems.protocol = PROTOCOLS::SENDSTATES;
-            
-            return ems; 
-        }
-
-        int insertState(DynamicMasterMessage &dmm){
-            DeviceID devName = dmm.info.device; 
-            this->deviceInfo[devName].insertState(dmm); 
-            int triggerID = 0; 
-            bool onSuccess = this->triggerMan.processDevice(devName, triggerID); 
-            // user trigger id to calculate priority
-            int trig = 10; 
-            if(onSuccess){
-                return trig; 
-            }
-            return -1; 
-        }
+struct IdentData{
+    std::unordered_map<OblockID, int> oblockMap; 
+    std::unordered_map<DeviceID, int> deviceMap; 
+    std::unordered_map<int, DeviceID> intToDev; 
 }; 
 
-
-
-
-
-// Exec unit processor (with mailbox and EU combined)
+// Client side EU
 class ClientEU{
     private: 
-        ClientEUCache dataQueue; 
-        TSQ<OwnedSentMessage>& clientTransfer; 
-
-        // FINISH
-        void transform(EMStateMessage &ems){
-            if(ems.protocol == PROTOCOLS::SENDSTATES){
-                auto& dmmList = ems.dmm_list; 
-                
-
-                
-            }
-        }
+        BlsLang::VirtualMachine virtualMachine; 
+        int bytecodeOffset; 
+        ReaderBox EuCache; 
+        TSQ<EMStateMessage> reciever; 
+        DeviceScheduler &scheObj; 
+        OblockID name; 
+        OBlockDesc oinfo; 
+        TSM<DeviceID, HeapMasterMessage> replacementCache; 
+        TSQ<SentMessage> &clientMainLine; 
+        int ctlCode; 
+        std::unordered_map<DeviceID, int> devPosMap; 
+        IdentData &idMaps; 
+        
+        void replaceCache(std::unordered_map<DeviceID, HeapMasterMessage> &currentLoad); 
         
     public: 
-        ClientEU(OBlockDesc &odesc, TSQ<OwnedSentMessage> &inMsg)
-        : dataQueue(odesc), clientTransfer(inMsg) {}
+        ClientEU(OBlockDesc &odesc, DeviceScheduler &scheObj, TSQ<SentMessage> &clientMainLine, 
+        IdentData &idData, int ctlCode);
 
-        // FINISH
-        void loadBytecode(std::vector<char> &bytecode){
-            
-        }
-
-        // FINISH
-        bool insertState(DynamicMasterMessage &dmm){
-            int trigPr = this->dataQueue.insertState(dmm);
-            if(trigPr != -1){
-                EMStateMessage emsMsg = this->dataQueue.grabStates(trigPr); 
-                this->transform(emsMsg); 
-            }
-        }
+        void insertDevice(HeapMasterMessage heapDesc);
+        void run(); 
+        SentMessage createSentMessage(BlsType &hdesc, DeviceDescriptor &devDesc, Protocol pcode);
 }; 
 
-
-// Kind of the exeuction manager of the client
-class ClientGateway{
-    
+class ClientEM{
     private: 
-        std::unordered_map<OblockID, ClientEU> execUnits; 
-        DeviceScheduler scheduler; 
-        TSQ<OwnedSentMessage> &inMsg; 
-        int controllerID; 
+        // Device to oblock list (using the in devices)
+        std::unordered_map<DeviceID, std::vector<OblockID>> devToOblockMap; 
+
+        // Add a loopback queue that is a heap descriptor by default
+        DeviceScheduler clientScheduler; 
+        TSQ<SentMessage> &clientReadLine; 
         
+        // The connection outLine
+        TSQ<SentMessage> &clientWriteLine; 
+    
+        int ctlCode; 
+        std::unordered_map<OblockID, std::unique_ptr<ClientEU>> clientMap; 
+        IdentData ident_data; 
 
-        void convSendMessage(DynamicMasterMessage dmm){
-            OwnedSentMessage osm;
-            // Fix this later
-            osm.connection = nullptr; 
-        
-            osm.sm.prot = Protocol::BEGIN; 
-            osm.sm.ctl_code = dmm.info.controller; 
-            osm.sm.oblock_id = dmm.info.oblock;    
-        } 
-
-
+        // Converts sent message to HMM
+        HeapMasterMessage getHMM(SentMessage &toConvert, PROTOCOLS pcol);
+    
     public: 
-        ClientGateway(std::vector<OBlockDesc>& oDescList, TSQ<OwnedSentMessage>& inMsgA, int selfID)
-        : scheduler(oDescList, [this](DynamicMasterMessage dmm){}), inMsg(inMsgA)
-        {
-            this->controllerID = selfID; 
-            for(auto& desc: oDescList){
-                this->execUnits.emplace(desc, ClientEU(desc, inMsg)); 
-            }
-        }
+        ClientEM(std::vector<OBlockDesc> &descList, 
+             TSQ<SentMessage> &readLine, 
+             TSQ<SentMessage> &writeLine, 
+             std::unordered_map<DeviceID, int> deviceMap, 
+             int ctlCode); 
 
-
+        void run(); 
 }; 
+
+
+        
