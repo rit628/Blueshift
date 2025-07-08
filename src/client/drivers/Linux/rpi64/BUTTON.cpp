3d7a5353fdad8b3e8ad58fc2f1f9fc5fbf192e40
Tomas Rivera <tr4462603@gmail.com>
2025-07-08 14:22:57 -0400

    drivers: Fix BUTTON debounce interrupts

--- src/client/drivers/Linux/rpi64/BUTTON.cpp
+++ src/client/drivers/Linux/rpi64/BUTTON.cpp
@@ -6,22 +6,19 @@
 using namespace Device;
 
 bool BUTTON::handleInterrupt(int gpio, int level, uint32_t tick) {
-    // static auto DEBOUNCE_TIME = std::chrono::microseconds(10);
-    if (level == RISING_EDGE) {
+    if (level == PI_TIMEOUT) {
+        std::cout << "FUCK YOU IM THE VILLAN" << std::endl;
+        return false;
+    }
+    else if (level == RISING_EDGE) {
         states.pressed = true;
+        return true;
     }
     else if (level == FALLING_EDGE) {
         states.pressed = false;
+        return true;
     }
-    else {
-        return false;
-    }
-    // auto currPress = std::chrono::high_resolution_clock::now();
-    // bool ret = (currPress - lastPress) > DEBOUNCE_TIME;
-    // lastPress = currPress;
-    // return ret;
-    return true;
-
+    return false;
 }
 
 void BUTTON::init(std::unordered_map<std::string, std::string> &config) {
@@ -34,6 +31,7 @@ void BUTTON::init(std::unordered_map<std::string, std::string> &config) {
     }
     gpioSetMode(this->PIN, PI_INPUT);
     gpioSetPullUpDown(this->PIN, PI_PUD_UP);
+    gpioGlitchFilter(this->PIN, 50*1000);
 
     auto bound = std::bind(&BUTTON::handleInterrupt, std::ref(*this), std::placeholders::_1, std::placeholders::_2, std::placeholders::_3); 
 
