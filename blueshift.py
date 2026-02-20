c2d0e642fcb1f421870fe095bb7970f2b3f066e1
Tomas Rivera <tr4462603@gmail.com>
2026-02-19 20:29:47 -0500

    build: Add diagnostic messages to reset subcommand

--- blueshift.py
+++ blueshift.py
@@ -391,32 +391,40 @@ def test(args):
         run_cmd(["docker", "compose", "run", "--rm", "builder", "test", "-l", *ctest_args])
 
 def reset(args):
+    print("Removing build directory and venv...")
     rmtree(BUILD_OUTPUT_DIRECTORY, ignore_errors=True)
     rmtree(".venv", ignore_errors=True)
     
     if args.preserve_images: return
     
+    print("Shutting down all containers...")
     run_cmd(["docker", "compose", "down", "--rmi", "all", "-v", "--remove-orphans"])
 
     network_id = get_output(f"docker network ls -f 'name={NETWORK_NAME}' --format '{{{{.ID}}}}'", shell=True)
     container_ids = get_output(f"docker container ls -af 'name={PROJECT_PREFIX}-*' --format '{{{{.ID}}}}'", shell=True)
     image_ids = get_output(f"docker image ls -af 'reference={PROJECT_PREFIX}-*' --format '{{{{.ID}}}}'", shell=True)
     volumes = get_output(f"docker volume ls -f 'name={PROJECT_PREFIX}_*' --format '{{{{.Name}}}}'", shell=True)
-        
+    
     if network_id:
+        print("Removing dangling networks...")
         run_cmd(["docker", "network", "rm", network_id])
     if container_ids:
+        print("Removing dangling containers...")
         run_cmd(["docker", "container", "stop"] + container_ids.split('\n'), exit_on_failure=False)
         run_cmd(["docker", "container", "rm", "-f"] + container_ids.split('\n'))
     if image_ids:
+        print("Removing dangling images...")
         run_cmd(["docker", "image", "rm", "-f"] + image_ids.split('\n'))
     if volumes:
+        print("Removing dangling volumes...")
         run_cmd(["docker", "volume", "rm", "-f"] + volumes.split('\n'))
 
     if args.system_prune:
+        print("Pruning everything in current docker context...")
         run_cmd(["docker", "system", "prune", "--volumes", "-af"])
 
     if args.rm_build_cache:
+        print("Removing dangling image build cache for current docker context...")
         run_cmd(["docker", "buildx", "prune", "-f"])
 
 parser = argparse.ArgumentParser(description=f"{PROJECT_NAME} development environment build script", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
