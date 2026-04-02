a7f0e7c29908f12a21e6bac9bd2843bbcdee05cd
Tomas Rivera <tr4462603@gmail.com>
2026-04-01 22:45:03 -0400

    build: Enable filesystem and exception support for wasm32

--- .cmake/wasm32.cmake
+++ .cmake/wasm32.cmake
@@ -2,13 +2,15 @@ set(EMSCRIPTEN_FORCE_COMPILERS OFF)
 include(/wasm32/cmake/Modules/Platform/Emscripten.cmake) # use emscripten toolchain as base
 
 # COMPILER CONFIG
-add_compile_options(-pthread) # enable threads
+add_compile_options(-pthread -fwasm-exceptions) # enable threads and exception support
+add_link_options(-fwasm-exceptions -sNODERAWFS=1) # exceptions and filesystem
 
 # LINKER CONFIG
 set(CMAKE_LINKER_TYPE DEFAULT) # use wasm-ld
 
 # CONDITIONAL COMPILATION VARIABLES
 add_compile_definitions(CONTROLLER_TARGET="WEB")
+add_compile_definitions(__WASM32__)
 
 # temporary hack to get asio to build; probably breaks at runtime
 set(BOOST_CONTEXT_IMPLEMENTATION "ucontext" CACHE INTERNAL "Boost.Context implementation (fcontext, ucontext, winfib)" FORCE)
