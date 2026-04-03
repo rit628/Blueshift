b3afef2d41a21d757b3e8bfdcdd1e5cf78fa64ea
Tomas Rivera <tr4462603@gmail.com>
2026-04-03 12:12:08 -0400

    build: Update wasm32-web target

--- .cmake/wasm32-web.cmake
+++ .cmake/wasm32-web.cmake
@@ -2,5 +2,14 @@ include(${CMAKE_CURRENT_LIST_DIR}/wasm32.cmake) # use wasm32 toolchain as base
 set(CMAKE_EXECUTABLE_SUFFIX_C ".html")
 set(CMAKE_EXECUTABLE_SUFFIX_CXX ".html")
 
+# COMPILER CONFIG
+add_link_options(
+                    -lidbfs.js # LDBFS for persistent storage in the browser
+                    -sEXPORT_ES6=1 # Create ES6 modules
+                    -sEXIT_RUNTIME=1 # Runtimes will exit on end of main()
+                    -sFORCE_FILESYSTEM # Filesystem support
+                    -sEXPORTED_RUNTIME_METHODS=['FS','IDBFS'] # Filesystem objects
+                )
+
 # CONDITIONAL COMPILATION VARIABLES
 add_compile_definitions(CONTROLLER_TARGET="WEB")
