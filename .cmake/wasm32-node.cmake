a2fa5b4f87e507fe9438388193a77f21b54543b9
Tomas Rivera <tr4462603@gmail.com>
2026-04-05 18:35:36 -0400

    build: Disable sdl for node target

--- .cmake/wasm32-node.cmake
+++ .cmake/wasm32-node.cmake
@@ -1,7 +1,12 @@
 include(${CMAKE_CURRENT_LIST_DIR}/wasm32.cmake) # use wasm32 toolchain as base
 
 # COMPILER CONFIG
+set(CMAKE_EXECUTABLE_SUFFIX_C ".js")
+set(CMAKE_EXECUTABLE_SUFFIX_CXX ".js")
 add_link_options(-sNODERAWFS=1) # filesystem support
 
 # CONDITIONAL COMPILATION VARIABLES
 add_compile_definitions(CONTROLLER_TARGET="NODEJS")
+
+# DISABLED LIBS
+set(DISABLE_SDL true) # SDL doesnt work with node for some reason
