30acc7e5a5e5e371f741acf5ceb9ceccd8366b28
Tomas Rivera <tr4462603@gmail.com>
2026-04-06 13:58:24 -0400

    build: Update web target to temporarily disable SDL3

--- .cmake/wasm32-web.cmake
+++ .cmake/wasm32-web.cmake
@@ -7,14 +7,19 @@ add_link_options(
                     -sPTHREAD_POOL_SIZE=navigator.hardwareConcurrency # set thread count to core count
                     -sPTHREAD_POOL_SIZE_STRICT=0 # allow thread pool exhaustion
 
-                    -sEXPORT_ES6=1 # Create ES6 modules
+                    -lwebsocket.js # access to emscripten websocket api for networking
 
-                    -sEXIT_RUNTIME=1 # Runtimes will exit on end of main()
+                    -sEXPORT_ES6=1 # create ES6 modules
+
+                    -sEXIT_RUNTIME=1 # runtimes will exit on end of main()
                     
-                    -sFORCE_FILESYSTEM # Filesystem support
+                    -sFORCE_FILESYSTEM # filesystem support
                     -lidbfs.js # LDBFS for persistent storage in the browser
-                    -sEXPORTED_RUNTIME_METHODS=['FS','IDBFS'] # Filesystem objects
+                    -sEXPORTED_RUNTIME_METHODS=['FS','IDBFS'] # filesystem objects
                 )
 
 # CONDITIONAL COMPILATION VARIABLES
 add_compile_definitions(CONTROLLER_TARGET="WEB")
+
+# DISABLED LIBS
+set(DISABLE_SDL true) # SDL3 currently broken on web
